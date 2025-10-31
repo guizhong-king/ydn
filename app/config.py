fc611d024310041060e96d76f7478f94294c8c35
guizhong-king <1716544487@qq.com>
2025-10-31 16:33:26 +0800

    Update config.py

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@
 import os
 
 # URL 和 浏览器路径优先从环境变量读取
-URL = os.environ.get('AUTO_URL', "https://pplx.ai/171654448770452")
+URL = os.environ.get('AUTO_URL', "https://pplx.ai/351806712787926")
 
 # Windows 系统下默认下载目录：C:\Users\用户名\Downloads
 if os.name == 'nt':
